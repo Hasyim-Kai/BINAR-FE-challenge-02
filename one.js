07a175bd2684bdd6734dae72d936d905bc07c660
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-01 20:32:57 +0700

    feat: one.js complete

--- one.js
+++ one.js
@@ -1,2 +1,7 @@
-console.log(`console.log('Contoh aja ya kan')`)
-console.log(`Hasil`)
+const changeWord = ( selectedText, changedText, text ) => text.replace( selectedText, changedText )
+
+const kalimat1 = `Andini sangat mencintai kamu selamanya`
+const kalimat2 = `Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu`
+
+console.log(changeWord('mencintai', 'membenci', kalimat1))
+console.log(changeWord('bromo', 'semeru', kalimat2))

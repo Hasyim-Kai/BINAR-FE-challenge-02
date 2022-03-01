c30aa17a67b103731d892cb4114720867fd4cce6
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-01 20:48:53 +0700

    feat: two.js complete

---
+++ two.js
@@ -0,0 +1,16 @@
+const checkTypeNumber = givenNumber => {
+    if(givenNumber == null){
+        return `Error: Bro where is the parameter ?`
+    } else if (typeof givenNumber != 'number'){   
+        return `Error: Invalid data type`
+    } else if(typeof givenNumber == 'number'){
+        return givenNumber % 2 === 1 ? `GANJIL` : `GENAP`
+    }
+}
+
+console.log(checkTypeNumber(10))
+console.log(checkTypeNumber(3))
+console.log(checkTypeNumber('3'))
+console.log(checkTypeNumber({}))
+console.log(checkTypeNumber([]))
+console.log(checkTypeNumber())

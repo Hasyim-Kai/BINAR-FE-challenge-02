ec1241252f6371c822b8748cf6c2787264dd7c11
ichihatsu1 <ichihatsu1@gmail.com>
2022-03-04 19:44:09 +0700

    feat: four.js complete

--- four.js
+++ four.js
@@ -1,28 +1,26 @@
-// const isValidPassword = givenPassword => {
-//     if (givenPassword == null) {
-//         return `Error: input yg diberikan kosong`
-//     } else if (typeof givenPassword != 'string') {
-//         return `Error: Invalid data type. Parameter type must be string`
-//     } else if (typeof givenPassword == 'string') {
-//         const REGEX_EMAIL = /[\w]+@[a-z]+\.[a-z]/
+const isValidPassword = givenPassword => {
+    if (givenPassword == null) {
+        return `Error: input yg diberikan kosong`
+    } else if (typeof givenPassword != 'string') {
+        return `Error: Invalid data type. Parameter type must be string`
+    } else if (typeof givenPassword == 'string') {
+        if(givenPassword.length < 8){
+            return false
+        } else if(!/[A-Z]{1,}/.test(givenPassword)){
+            return false
+        } else if(!/[a-z]{1,}/.test(givenPassword)){
+            return false
+        } else if(!/[0-9]{1,}/.test(givenPassword)){
+            return false
+        } else {
+            return true
+        }
+    }
+}
 
-//         if(givenPassword.length < 8){
-//             return 'Error: karakter harus lebih dari 8'
-//         } else if(){
-
-//         } else if(){
-            
-//         } else if(){
-            
-//         } else if(){
-            
-//         }
-//         return REGEX_EMAIL.test(givenPassword) ? `VALID` : `INVALID`
-//     }
-// }
-
-// console.log(isValidPassword('apranata@binar.co.id'))
-// console.log(isValidPassword('apranata@binar.com'))
-// console.log(isValidPassword('apranata@binar'))
-// console.log(isValidPassword('apranata'))
-// console.log(isValidPassword())
+console.log(isValidPassword('Meong2021'))
+console.log(isValidPassword('meong2021'))
+console.log(isValidPassword('@eong'))
+console.log(isValidPassword('Meong2'))
+console.log(isValidPassword(0))
+console.log(isValidPassword())
